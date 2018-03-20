36f7d641f6c1fc40f3cf93ab868dd3949fca2708
Janakiraman <ramramu.jr@gmail.com>
2018-03-20 22:27:11 +0530

    java_app

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
-nohup java -jar  /home/ubuntu/spring-boot-web-jsp-1.0.war &
-echo $! >  /home/ubuntu/pid
+nohup java -jar  /home/ubuntu/java_app/spring-boot-web-jsp-1.0.war &
+echo $! >  /home/ubuntu/java_app/pid
