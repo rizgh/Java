324ac1659a65241c7e1179b3293cb203ba5789ae
Janakiraman <ramramu.jr@gmail.com>
2018-03-20 22:36:42 +0530

    test-codedeploy

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ -f /home/ubuntu/java_app/pid ]; then
 cat /home/ubuntu/java_app/pid | xargs kill
+rm /home/ubuntu/java_app/spring-boot-web-jsp-1.0.war
 else
 echo "new installation available"
 fi
